1dbf93e8d92cf6dbda582d7413c8c90b9f692ec2
Ayusht777 <talesarayush@gmail.com>
2023-04-12 22:39:06 +0530

    day-203

---
+++ DAY-203/script.js
@@ -0,0 +1,36 @@
+console.log("hello Js");
+
+var num = 101;
+
+var str = "hello world";
+
+var bool = false;
+
+var none = null;
+console.log(num);
+
+console.log(str);
+
+num = 70;
+
+console.log(num);
+
+console.log(bool);
+
+console.log("str");
+
+const v = 100;
+
+// v = 101;
+
+let NewNum = 105;
+
+console.log(NewNum);
+
+NewNum = NewNum - 10;
+
+// let NewNum = 104;
+
+console.log(NewNum);
+
+
